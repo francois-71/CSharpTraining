e18c478d9587305a7a046ae60cda908000fd476f
Francois Dion <francoisdion71@gmail.com>
2024-11-03 21:46:10 +0100

    Revert "first mini-project"
    This reverts commit 17a3fa21d84a6a2ab5508753cbf890665adc1c7b.

--- AvailabilityStatusEnum.cs
+++
@@ -1,7 +0,0 @@
-namespace enums;
-
-public enum AvailabilityStatus
-{
-    AVAILABLE,
-    NOT_AVAILABLE,
-}
--- CreationTypeEnum.cs
+++
@@ -1,9 +0,0 @@
-namespace enums;
-
-public enum TransactionType
-{
-    REGISTER_BOOK,
-    REGISTER_USER,
-    BORROW_BOOK,
-    RETURN_BOOK,
-}

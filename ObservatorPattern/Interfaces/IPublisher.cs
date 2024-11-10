65d699ab894b776e0401ba105a0cd5b6205bfec3
Francois Dion <francoisdion71@gmail.com>
2024-11-10 14:15:48 +0100

    deleted pattern copy

--- ObservatorPattern/Interfaces/IPublisher.cs
+++
@@ -1,8 +0,0 @@
-namespace Interfaces
-{
-    // interface all subscriber should implement so the publisher can send a message to all it's subscribers.
-    public interface INotification
-    {
-        public void Update(string updateMessage);
-    }
-}
